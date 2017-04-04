9ab54fb598293ffa702fc879eb8cbaf98d2278a1
HUPENG <hupeng@imudges.com>
2017-04-04 22:08:25 +0800

    update:bug修复

--- src/me/hupeng/java/video2text/VideoUtil.java
+++ src/me/hupeng/java/video2text/VideoUtil.java
@@ -128,6 +128,7 @@ public class VideoUtil{
                 e.printStackTrace();
             }
         }
+        deleteDir(tempDirFile);
         return text;
     }
 
@@ -372,4 +373,24 @@ public class VideoUtil{
         return min;
     }
 
+    /**
+     * 递归删除目录下的所有文件及子目录下所有文件
+     * @param dir 将要删除的文件目录
+     * @return boolean Returns "true" if all deletions were successful.
+     *                 If a deletion fails, the method stops attempting to
+     *                 delete and returns "false".
+     */
+    private static boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i=0; i<children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        // 目录此时为空，可以删除
+        return dir.delete();
+    }
 }
